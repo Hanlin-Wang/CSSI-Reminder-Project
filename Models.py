fc1d6972766b3d8a65a839caf2d7d270de7c8d52
Gursharn <gursharns2000@gmail.com>
2018-08-09 15:55:45 -0400

    Add variables to store

--- Models.py
+++ Models.py
@@ -1,3 +1,6 @@
 from google.appengine.ext import ndb
 
-# class ReminderData(ndb.Model):
+class ReminderData(ndb.Model):
+    optionwater = ndb.StringProperty(required=False)
+    optionshave = ndb.StringProperty(required=False)
+    optionsleep = ndb.StringProperty(required=False)
